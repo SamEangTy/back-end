babec06ed587231d1315226409e45909d61febef
SamEangTy <121166611+SamEangTy@users.noreply.github.com>
2023-07-04 21:12:27 +0700

    login

---
+++ app/Http/Controllers/ControllerUser.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+
+class ControllerUser extends Controller
+{
+    function login (Request $req){
+        $user = User::where('email',$req->email)->first();
+        if(!$user){
+            return ["message" =>"Email incorrect!"];
+        } else if(!Hash::check($req->password,$user->password)){
+            return ["message" =>"Password incorrect!"];
+        } else{
+            return ['message'=>'Login Success',$user];
+        }
+    }
+    function register (Request $req){
+       $user = User::where('email',$req->email)->first();
+       if($user){
+        return ['message'=>'User existing! Please sign up another email.'];
+       }else{
+           $req->validate([
+            'name' => 'required|string',
+            'email' => 'required|string|unique:users',
+            'password' => 'required|string|min:6',
+           ]);
+    
+           $user = new User([
+            'name' => $req->name,
+            'email' => $req->email,
+            'password' => Hash::make($req->password)
+           ]);
+           $user->save();
+           return response()->json(['message'=>'User has been registered',$user]);
+       }
+    }
+}
