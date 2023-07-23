9065355f51e78d06ebbc74b727fa91716412c424
SamEangTy <121166611+SamEangTy@users.noreply.github.com>
2023-07-23 10:16:39 +0700

    update and delete product

--- app/Http/Controllers/ControllerProduct.php
+++ app/Http/Controllers/ControllerProduct.php
@@ -4,8 +4,68 @@ namespace App\Http\Controllers;
 
 use App\Models\Product;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class ControllerProduct extends Controller
 {
-  
+  function addProduct (Request $req)
+  {
+   $product = new Product;
+//    $price = DB::table('prices')->insert(['price'=>$req->price,'discount'=>$req->discount]);
+//    $categery = DB::table('categeries')->insert(['categery_name'=>$req->categery_name]);
+   $product->name = $req->input('name');
+   $product->categery_id = $req->input('categery_id');
+
+   $product->price_id = $req->input('price_id');
+//    $product->discount = $req->input('discount');
+   $product->image = $req->file('image')->store('product/products');
+   $product->save();
+   return $product;
+  }
+  function listProduct ()
+    //   $discount = DB::table('products')
+    //   ->select(DB::raw('price * (discount/100) - price AS discountPrice'))
+    //   ->get();
+    {
+        $products = DB::select("
+        SELECT price - price * (discount/100) AS discountPrice,
+        id, name, price, discount, image,
+        categeries.categery_name  FROM `products`
+        INNER JOIN prices on  products.price_id = prices.price_id 
+        INNER JOIN categeries on products.categery_id = categeries.categery_id");
+      // $products = DB::table('products')
+            
+      //    //  ->select(DB::raw('price * (discount/100) - price AS discountPrice'))
+      //     ->join('categeries','categeries.categery_id','=','products.categery_id')
+      //     ->join('prices','prices.price_id','=','products.price_id')
+      //    //  ->select('products.*', 'products')
+      //   ->get(['id','categery_name','name','price','discount','image']);
+        return  $products;
+    }
+    function updateProduct (Request $req, $id)
+    {
+      $product = Product::find($id);
+      if($product){
+        $product->update($req->all());
+        $product->image = $req->file('image')->store('product/products');
+        $product->save();
+        return ["message"=>'Product Update Success', $product];
+      }else{
+        return ["message"=>'Product ID not found'];
+      }
+    }
+    function deleteProduct ($id)
+    { 
+      $product = Product::find($id);
+      $deleteProduct = Product::where('id',$id)->delete();
+      if($deleteProduct)
+      {
+
+        return ['message'=>'Product delete succes','Product has been delete'=>$product];
+      }else
+      {
+        return ['message'=>'Product not found!'];
+
+      }
+    }
 }
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\ControllerHome;
+use App\Http\Controllers\ControllerProduct;
 use App\Http\Controllers\ControllerUser;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -19,7 +20,14 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+//login route
 Route::post("login",[ControllerUser::class,'login']);
 Route::post("register",[ControllerUser::class,'register']);
+// home route
 Route::get("homepage",[ControllerHome::class,'homepage']);
 Route::post("update-home/{id}",[ControllerHome::class,'updateHome']);
+// product route
+Route::get("listProduct",[ControllerProduct::class,'listProduct']);
+Route::post("addProduct",[ControllerProduct::class,'addProduct']);
+Route::post("updateProduct/{id}",[ControllerProduct::class,'updateProduct']);
+Route::delete("deleteProduct/{id}",[ControllerProduct::class,'deleteProduct']);
